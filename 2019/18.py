17ccd87b7c01f2fd6370a4f300a96668ca63beb5
Shun Git <kwokshungit@gmail.com>
2019-12-21 21:01:28 +0800

    day 18

---
+++ 2019/18.py
@@ -0,0 +1,135 @@
+from utils import dijkstra
+
+directions = ((1, 0), (0, -1), (-1, 0), (0, 1))
+
+
+class Maze:
+
+    def __init__(self, lines):
+        self.items = {}
+        self.keys = set()
+        self.doors = set()
+        self.walls = set()
+
+        start = set()
+        for y, line in enumerate(lines):
+            for x, char in enumerate(line):
+                pos = (x, y)
+                if char == '#':
+                    self.walls.add(pos)
+                elif char == '.':
+                    pass
+                elif char == '@':
+                    start.add(pos)
+                else:
+                    self.items[pos] = char.lower()
+                    if char.isupper():
+                        self.doors.add(pos)
+                    else:
+                        self.keys.add(pos)
+
+        self.numkeys = len(self.keys)
+        self.start = {i: v for i, v in enumerate(start)}
+
+    def compute_graph(self):
+        self.graph = {i: dict(self.key_dist(p)) for i, p in self.start.items()}
+        for key in self.keys:
+            self.graph[self.items[key]] = dict(self.key_dist(key))
+
+    def neighbours(self, node, current_dist):
+        x, y = node
+        newdist = current_dist + 1
+        for dx, dy in directions:
+            newpos = (x + dx, y + dy)
+            if newpos in self.walls:
+                continue
+            yield newpos, newdist
+
+    def key_dist(self, position):
+        distances, paths = dijkstra(position, self.neighbours, paths=True)
+        for key in (distances.keys() & self.keys):
+            if key == position:
+                continue
+            doors = {self.items[d] for d in self.doors.intersection(paths[key])}
+            yield self.items[key], (distances[key], doors)
+
+
+def update_for_part_2(maze):
+    i = 0
+    sx, sy = maze.start.pop(0)
+    for dy in (-1, 0, 1):
+        for dx in (-1, 0, 1):
+            pos = (sx + dx, sy + dy)
+            if (not dx) or (not dy):
+                maze.walls.add(pos)
+            else:
+                maze.start[i] = pos
+                i += 1
+
+
+def maze_neighbour(func, max_nodes=5, cull_factor=1.6):
+    best_dist = {}
+
+    def f(node, current_dist):
+        numkeys = len(node[0])
+        best = best_dist.get(numkeys)
+        if best is None or current_dist < best:
+            best_dist[numkeys] = current_dist
+
+        newbest = best_dist.get(numkeys + 1)
+        cull = (newbest * cull_factor) if newbest else 0
+
+        keys = set(node[0])
+        nodes = sorted(func(node, current_dist))
+        for dist, key, *state in nodes[:max_nodes]:
+            if cull and dist > cull:
+                continue
+            yield (''.join(sorted({key} | keys)), *state), dist
+
+    return f
+
+
+if __name__ == '__main__':
+    with open('input/18.txt') as f:
+        maze = Maze(line.strip() for line in f)
+
+    def found(node, distances):
+        return len(node[0]) == maze.numkeys
+
+    # part 1
+    maze.compute_graph()
+    free_keys = sum(1 for d, k in maze.graph[0].values() if not k)
+
+    @maze_neighbour
+    def simple(node, current_dist):
+        keys = set(node[0])
+        for key, info in maze.graph[node[1]].items():
+            if key in keys or (info[1] - keys):
+                continue
+            yield (current_dist + info[0], key, key)
+
+    dists_1 = dijkstra(('', 0), simple, stop=found)
+    min_d = min(d for s, d in dists_1.items() if len(s[0]) == maze.numkeys)
+    print(min_d)
+
+    # part 2
+    update_for_part_2(maze)
+    maze.compute_graph()
+    best_dist = {}
+
+    @maze_neighbour
+    def robots(node, current_dist):
+        keys = set(node[0])
+        botpos = node[1:]
+        for i, pos in enumerate(botpos):
+            for key, info in maze.graph[pos].items():
+                if key in keys or (info[1] - keys):
+                    continue
+                bots = list(botpos)
+                bots[i] = key
+                yield (current_dist + info[0], key, *bots)
+
+
+    dists_2 = dijkstra(('', *maze.start), robots, stop=found)
+    min_d = min(d for s, d in dists_2.items() if len(s[0]) == maze.numkeys)
+    print(min_d)
--- 2019/utils.py
+++ 2019/utils.py
@@ -1,4 +1,23 @@
-def dijkstra(src, neighbours, dst=None):
+class Paths:
+
+    def __init__(self, tree):
+        self.tree = tree
+
+    def __getitem__(self, key):
+        return tuple(reversed(tuple(self.walk(key))))
+
+    def walk(self, key):
+        while key:
+            key = self.tree.get(key)
+            if key:
+                yield key
+
+    def __repr__(self):
+        return '<Paths(%d)>' % len(self.tree)
+
+
+def dijkstra(src, neighbours, dst=None, stop=None, paths=False):
+    parents = {}
     visited = set()
     unvisited = {src}
     distances = {src: 0}
@@ -11,12 +30,25 @@ def dijkstra(src, neighbours, dst=None):
         for node, newdist in neighbours(current, distances[current]):
             olddist = distances.get(node)
             if olddist is None or newdist < olddist:
+                parents[node] = current
                 distances[node] = newdist
+
         if current == dst:
             break
+
+        try:
+            if stop(current, distances):
+                break
+        except TypeError:
+            pass
+
         visited.add(current)
         unvisited = distances.keys() - visited
 
     if dst is not None:
         return distances[dst]
+
+    if paths:
+        return distances, Paths(parents)
+
     return distances
